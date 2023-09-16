8026685834c24f1fee40c76edd1191b27fb62ee0
Bogumił <hello@milewski.me>
2023-09-16 11:05:43 +0200

    Use dot notation for Heading in Markdown

--- next.js/src/utils/Markdown.js
+++ next.js/src/utils/Markdown.js
@@ -18,14 +18,15 @@ const LinkRenderer = ({ href, children }) => {
 
 const ListRenderer = ({ children, ordered }) => (
   <li>
-    {!ordered && <ListBullet />}
+    {!ordered && '-'}
     <span>{children}</span>
   </li>
 )
 
 const Markdown = ({ level, children, components, ...props }) => {
   const HeadingComponent = level;
-  const updatedComponents = level
+  const updatedComponents =
+    level
     ? {
         ...components,
         p: ({ children }) => <HeadingComponent {...props}>{children}</HeadingComponent>
@@ -55,21 +56,11 @@ const Markdown = ({ level, children, components, ...props }) => {
   );
 }
 
-const ListBullet = () => (
-  <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' viewBox='0 0 24 24'>
-    <path
-      fill='#2E445A'
-      d='M4 11.25a.75.75 0 000 1.5v-1.5zm0 1.5h16v-1.5H4v1.5z'
-      opacity='0.5'
-    ></path>
-    <path
-      stroke='#2E445A'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      strokeWidth='1.5'
-      d='M14 6l6 6-6 6'
-    ></path>
-  </svg>
-)
+Markdown.h1 = (props) => <Markdown level="h1" {...props} />;
+Markdown.h2 = (props) => <Markdown level="h2" {...props} />;
+Markdown.h3 = (props) => <Markdown level="h3" {...props} />;
+Markdown.h4 = (props) => <Markdown level="h4" {...props} />;
+Markdown.h5 = (props) => <Markdown level="h5" {...props} />;
+Markdown.h6 = (props) => <Markdown level="h6" {...props} />;
 
 export default Markdown;
