c178dd91449554fefbef8b8f7d45d0e9b62d5624
Bogumił <hello@milewski.me>
2023-09-20 13:06:11 +0200

    added resend and serverless to contact Form

---
+++ next.js/app/api/send/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const email = {
+  from: 'Acme <onboarding@resend.dev>',
+  to: 'kryptonumstudio@gmail.com',
+}
+
+export async function POST() {
+  try {
+    await resend.emails.send({
+      from: email.from,
+      to: email.to,
+      subject: 'Hello world',
+      html: 'Hello!',
+    });
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    return NextResponse.json({ success: false });
+  }
+}
