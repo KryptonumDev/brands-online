731efe49dec3b559470efb58b958d01126c342b0
Bogumił <hello@milewski.me>
2023-09-28 16:53:20 +0200

    optimized render and added mouse mesh different options

--- next.js/src/components/sections/Hero/index.js
+++ next.js/src/components/sections/Hero/index.js
@@ -1,12 +1,9 @@
 'use client'
-import { Suspense } from 'react';
+import { lazy, useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import Markdown from '@/utils/Markdown';
 import Button from '@/components/atoms/Button';
-import { HeroRender } from '../homepage/HeroRender';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stage } from '@react-three/drei'
-import Loader from '@/components/atoms/Loader';
+const HeroRender = lazy(() => import('../homepage/HeroRender'));
 
 const Hero = ({
   data: {
@@ -15,6 +12,11 @@ const Hero = ({
     hero_Cta,
   }
 }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <section className={styles.wrapper}>
       <header>
@@ -23,23 +25,171 @@ const Hero = ({
         <Button data={hero_Cta} />
       </header>
       <div className={styles.renders}>
-        <Suspense fallback={<Loader />}>
-          <Canvas>
-            <Stage preset="rembrandt" intensity={1} environment="city">
-              <HeroRender />
-            </Stage>
-            <OrbitControls
-              enablePan={false}
-              enableZoom={false}
-              maxPolarAngle={Math.PI / 2}
-              minPolarAngle={Math.PI / 2}
-              autoRotate
-            />
-          </Canvas>
-        </Suspense>
+        {!isMounted ? null : (
+          <HeroRender />
+        )}
+        <Background />
       </div>
     </section>
   );
 };
+
+const Background = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='382'
+    height='382'
+    viewBox='0 0 382 382'
+    fill='none'
+  >
+    <rect
+      width='381.691'
+      height='381.691'
+      fill='url(#paint0_linear_528_1069)'
+      rx='12.784'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='1.826'
+      y='1.826'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='96.793'
+      y='1.826'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='191.759'
+      y='1.826'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='286.725'
+      y='1.826'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='1.826'
+      y='96.793'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='96.793'
+      y='96.793'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='191.759'
+      y='96.793'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='286.725'
+      y='96.793'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='1.826'
+      y='191.759'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='96.793'
+      y='191.759'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='191.759'
+      y='191.759'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='286.725'
+      y='191.759'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='1.826'
+      y='286.725'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='96.793'
+      y='286.725'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='191.759'
+      y='286.725'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <rect
+      width='93.14'
+      height='93.14'
+      x='286.725'
+      y='286.725'
+      fill='#fff'
+      rx='10.958'
+    ></rect>
+    <defs>
+      <linearGradient
+        id='paint0_linear_528_1069'
+        x1='376.759'
+        x2='0.759'
+        y1='379'
+        y2='-1'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#E8E8E8'></stop>
+        <stop offset='1' stopColor='#fff'></stop>
+      </linearGradient>
+    </defs>
+  </svg>
+)
 
 export default Hero;
--- next.js/src/components/sections/homepage/HeroRender/index.js
+++ next.js/src/components/sections/homepage/HeroRender/index.js
@@ -1,15 +1,34 @@
 import React, { useEffect } from "react";
-import { useGLTF, OrthographicCamera } from "@react-three/drei";
-import { useMotionValue, useSpring } from "framer-motion";
+import { useGLTF, OrthographicCamera, Stage, OrbitControls } from "@react-three/drei";
+import { useMotionValue, useSpring, useTransform } from "framer-motion";
 import { motion } from 'framer-motion-3d';
+import { Canvas } from "@react-three/fiber";
 
-export function HeroRender(props) {
+const HeroRender = () => {
   const { nodes } = useGLTF("/renders/homepage.gltf");
 
-  const mouse = {
-    x: useSpring(useMotionValue(0), { damping: 20 }),
-    y: useSpring(useMotionValue(0), { damping: 20 }),
+  const options = {
+    damping: 20
   }
+  const mouse = {
+    x: useSpring(useMotionValue(0), options),
+    y: useSpring(useMotionValue(0), options),
+  }
+
+  const mesh = {
+    0: {
+      x: useTransform(mouse.x, [0, 1], [0, 1]),
+      y: useTransform(mouse.y, [0, 1], [0, 1]),
+    },
+    1: {
+      x: useTransform(mouse.x, [0, 1], [0, -2]),
+      y: useTransform(mouse.y, [0, 1], [0, -2]),
+    },
+    2: {
+      x: useTransform(mouse.x, [0, 1], [0, 1.5]),
+      y: useTransform(mouse.y, [0, 1], [0, 1.5]),
+    },
+  };
 
   const handleMouseMove = ({ clientX, clientY }) => {
     const { innerWidth, innerHeight } = window;
@@ -26,84 +45,97 @@ export function HeroRender(props) {
   }, [])
 
   return (
-    <group {...props} dispose={null}>
-      <group scale={0.01}>
-        <OrthographicCamera
-          makeDefault={false}
-          far={100000}
-          near={0}
-          position={[-41.172, -49.506, 1000]}
-        />
-        <motion.group
-          position={[46.234, -33.072, 58.831]}
-          rotation={[-0.051, 0.146, -0.577]}
-          scale={[0.401, 0.356, 0.356]}
-          rotation-x={mouse.x}
-          rotation-y={mouse.y}
-        >
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.ź.geometry}
-            material={nodes.ź.material}
-            position={[-144.169, 153.683, -8]}
-          />
-        </motion.group>
-        <motion.group
-          position={[-34.902, -89.227, -10.524]}
-          rotation={[-0.438, 0.139, -1.773]}
-          scale={0.384}
-          rotation-x={mouse.x}
-          rotation-y={mouse.y}
-        >
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Shape_0.geometry}
-            material={nodes.Shape_0.material}
-            position={[0.855, -0.787, 0]}
-          />
-        </motion.group>
-        <spotLight
-          intensity={1}
-          angle={Math.PI / 6}
-          decay={2}
-          distance={2000}
-          position={[167.106, 183.826, -34.752]}
-          rotation={[-0.566, -0.228, -0.617]}
-          scale={[1, 0.903, 0.796]}
-        />
-        <group position={[-96.802, -74.177, 112.785]}>
-          <motion.group
-            position={[-43.814, -7.745, -19.598]}
-            rotation={[0.037, -0.152, 0.787]}
-            scale={0.366}
-            rotation-x={mouse.x}
-            rotation-y={mouse.y}
-          >
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={nodes.Shape_0_1.geometry}
-              material={nodes.Shape_0_1.material}
-              position={[0.427, -0.835, 0]}
+    <Canvas>
+      <Stage shadows={false}>
+        <group dispose={null}>
+          <group scale={0.01}>
+            <OrthographicCamera
+              makeDefault={false}
+              far={100000}
+              near={0}
+              position={[-41.172, -49.506, 1000]}
             />
-          </motion.group>
+            <motion.group
+              position={[46.234, -33.072, 58.831]}
+              rotation={[-0.051, 0.146, -0.577]}
+              scale={[0.401, 0.356, 0.356]}
+              rotation-x={mesh[0].x}
+              rotation-y={mesh[0].y}
+            >
+              <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.ź.geometry}
+                material={nodes.ź.material}
+                position={[-144.169, 153.683, -8]}
+              />
+            </motion.group>
+            <motion.group
+              position={[-34.902, -89.227, -10.524]}
+              rotation={[-0.438, 0.139, -1.773]}
+              scale={0.384}
+              rotation-x={mesh[1].x}
+              rotation-y={mesh[1].y}
+            >
+              <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Shape_0.geometry}
+                material={nodes.Shape_0.material}
+                position={[0.855, -0.787, 0]}
+              />
+            </motion.group>
+            <spotLight
+              intensity={1}
+              angle={Math.PI / 6}
+              decay={2}
+              distance={2000}
+              position={[167.106, 183.826, -34.752]}
+              rotation={[-0.566, -0.228, -0.617]}
+              scale={[1, 0.903, 0.796]}
+            />
+            <group position={[-96.802, -74.177, 112.785]}>
+              <motion.group
+                position={[-43.814, -7.745, -19.598]}
+                rotation={[0.037, -0.152, 0.787]}
+                scale={0.366}
+                rotation-x={mesh[2].x}
+                rotation-y={mesh[2].y}
+              >
+                <mesh
+                  castShadow
+                  receiveShadow
+                  geometry={nodes.Shape_0_1.geometry}
+                  material={nodes.Shape_0_1.material}
+                  position={[0.427, -0.835, 0]}
+                />
+              </motion.group>
+            </group>
+            <directionalLight
+              intensity={1}
+              decay={2}
+              rotation={[-0.342, 0.666, 0.523]}
+            />
+            <OrthographicCamera
+              makeDefault={false}
+              far={100000}
+              near={0}
+              position={[7.17, -30.83, 1000]}
+            />
+          </group>
         </group>
-        <directionalLight
-          intensity={1}
-          decay={2}
-          rotation={[-0.342, 0.666, 0.523]}
-        />
-        <OrthographicCamera
-          makeDefault={false}
-          far={100000}
-          near={0}
-          position={[7.17, -30.83, 1000]}
-        />
-      </group>
-    </group>
+      </Stage>
+      <OrbitControls
+        enablePan={false}
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+        autoRotate
+      />
+    </Canvas>
   );
 }
 
 useGLTF.preload("/renders/homepage.gltf");
+
+export default HeroRender;
