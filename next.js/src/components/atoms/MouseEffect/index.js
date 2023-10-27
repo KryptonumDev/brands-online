822cf7091b8c990f89bb2e5a5e235f6801cbc11a
Bogumił <hello@milewski.me>
2023-10-27 11:46:44 +0200

    removed goo filter from MouseEffect

--- next.js/src/components/atoms/MouseEffect/index.js
+++ next.js/src/components/atoms/MouseEffect/index.js
@@ -50,15 +50,6 @@ const MouseEffect = () => {
         <Blob.First />
         <Blob.Second />
         <Blob.Third />
-        <svg xmlns="http://www.w3.org/2000/svg" id={styles.goo}>
-          <defs>
-            <filter id="goo">
-              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
-              <feBlend in="SourceGraphic" in2="goo" />
-            </filter>
-          </defs>
-        </svg>
       </motion.div>
     </>
   );
