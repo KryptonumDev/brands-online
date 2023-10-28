fb1436d9f0c1d959f23009c7a8e1bef64d809695
Bogumił <hello@milewski.me>
2023-10-28 14:49:24 +0200

    added Switch to moleculas

---
+++ next.js/src/components/moleculas/Switch/index.js
@@ -0,0 +1,42 @@
+import styles from './styles.module.scss';
+
+const Switch = ({
+  ...props
+}) => {
+  return (
+    <label className={styles.toggle} {...props}>
+      <input
+        type='checkbox'
+      />
+      <div className={styles.dot}>
+        <Tick className={styles.tick} />
+      </div>
+    </label>
+  );
+};
+
+export default Switch;
+
+const Tick = ({ ...props }) => (
+  <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 17' fill='none' {...props}>
+    <path
+      stroke='url(#switch-gradient)'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M14 3.7l-8.4 9.6L2 9.7'
+    ></path>
+    <defs>
+      <linearGradient
+        id='switch-gradient'
+        x1='14'
+        x2='2'
+        y1='8.5'
+        y2='8.5'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#14E5D7'></stop>
+        <stop offset='1' stopColor='#00C2B5'></stop>
+      </linearGradient>
+    </defs>
+  </svg>
+)
